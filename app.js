2d2d79527ee451552ea82bf2830a337b671f4944
rogerespinosa <rogerespinosamorros@gmail.com>
2024-12-22 12:03:00 +0100

    conexión con mongodb

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { error } = require("console");
 const express = require("express");
 
 const mongoose = require("mongoose");
@@ -8,10 +9,22 @@ const PORT = 5000;
 const app = express();
 app.use(express.json());
 
-const url_mongo = process.env.DATABASE_URL_DEV;
-mongoose.connect(url_mongo);
+const url_mongodb = process.env.DATABASE_URL_DEV;
+mongoose.connect(url_mongodb);
 
 const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.log(`Error al conectar con mongo ${error}`);
+});
+
+db.on("connected", () => {
+  console.log(`Succes connect`);
+});
+
+db.on("disconnected", () => {
+  console.log(`Mongo is disconnected`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
