b7dbe012c490f5c7eacfd3d8185bd120bd94ae3e
rogerespinosa <rogerespinosamorros@gmail.com>
2024-12-22 11:39:53 +0100

    app.js no DB

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const PORT = 5000;
 const app = express();
 app.use(express.json());
 
+const url_mongo = process.env.DATABASE_URL_DEV;
+mongoose.connect(url_mongo);
+
+const db = mongoose.connection;
+
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
 });
